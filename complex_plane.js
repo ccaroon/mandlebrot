2b387cbb515614e287b747975b7c3257842daa90
Craig N. Caroon <ccaroon@webassign.net>
2014-03-13 12:29:26 -0400

    * Cleanup up code a bit. CamelCased some stuff, added spacing. * Added clear() method to complex_plane.js

--- complex_plane.js
+++ complex_plane.js
@@ -8,16 +8,24 @@ function ComplexPlane (w, h) {
   this._init();
 }
 //############################################################################
-ComplexPlane.prototype._init = function () {
+ComplexPlane.prototype._init = function (value) {
   var r, c;
+
+  if (value === undefined) {
+    value = ' ';
+  }
 
   for (r = 0; r < this.height; r+=1) {
     this.plane[r] = [];
     for (c = 0; c < this.width; c+=1) {
-      this.plane[r][c] = ' ';
+      this.plane[r][c] = value;
     }
   }
 
+};
+//##############################################################################
+ComplexPlane.prototype.clear = function (value) {
+  this._init(value);
 };
 //############################################################################
 ComplexPlane.prototype.set = function (x,y,value) {
--- mandlebrot.js
+++ mandlebrot.js
@@ -9,51 +9,53 @@ function Mandlebrot(xMin, xMax, yMin, yMax) {
 //##############################################################################
 Mandlebrot.prototype.compute = function (iterations, plane) {
   var x,y;
-  var xstep, ystep;
-  var z,zi,newz,newzi;
+  var xStep, yStep;
+  var z,zi,newZ,newZI;
   var colour;
-  var col;
   var i,j,k;
-  var inset;
+  var inSet;
 
   /* these are used for calculating the points corresponding to the pixels */
-  xstep = (this.xMax-this.xMin)/plane.width;
-  ystep = (this.yMax-this.yMin)/plane.height;
+  xStep = (this.xMax - this.xMin) / plane.width;
+  yStep = (this.yMax - this.yMin) / plane.height;
 
   x = this.xMin;
   y = this.yMin;
-  for (i=0; i<plane.height; i++)
+  for (i = 0; i < plane.height; i++)
   {
-    for (j=0; j<plane.width; j++)
+    for (j = 0; j < plane.width; j++)
     {
       z = 0;
       zi = 0;
-      inset = 1;
-      for (k=0; k<iterations; k++)
+      inSet = true;
+      for (k = 0; k < iterations; k++)
       {
         /* z^2 = (a+bi)(a+bi) = a^2 + 2abi - b^2 */
-        newz = (z*z)-(zi*zi) + x;
-        newzi = 2*z*zi + y;
-        z = newz;
-        zi = newzi;
-        if( ((z*z)+(zi*zi)) > 4 )
+        newZ = (z*z) - (zi*zi) + x;
+        newZI = 2*z*zi + y;
+        z = newZ;
+        zi = newZI;
+        if( ((z*z) + (zi*zi)) > 4 )
         {
-          inset = 0;
+          inSet = false;
           colour = k;
           k = iterations;
         }
       }
-      if (inset)
+
+      if (inSet)
       {
-        plane.set(i,j,'*');
+        plane.set(i,j,' ');
       }
       else
       { 
-        plane.set(i,j,' ');
+        plane.set(i,j,colour%9);
       }
-      x += xstep;
+
+      x += xStep;
     }
-    y += ystep;
+
+    y += yStep;
     x = this.xMin;
   }
 }
@@ -62,5 +64,14 @@ Mandlebrot.prototype.compute = function (iterations, plane) {
 var m = new Mandlebrot(-2,1,-1,1);
 var p = new ComplexPlane(160,60);
 
-m.compute(200, p);
-p.display();
+var i = 0;
+var doIt = function () {
+  console.log("=====> mandlebrot.js #69 --> i ["+i+"]");
+  m.compute(i, p);
+  p.display();
+  i+=1;
+  if (i < 50) {
+    setTimeout(doIt, 250);
+  }
+}
+doIt();
