f683154b65bebe4499170b6be026af569cdd0152
Craig N. Caroon <ccaroon@webassign.net>
2014-03-13 14:57:40 -0400

    * animate.js...changed var p to var c.

--- animate.js
+++ animate.js
@@ -3,12 +3,12 @@ var Mandlebrot = require('./mandlebrot');
 var AsciiCanvas = require('./ascii_canvas');
 //##############################################################################
 var m = new Mandlebrot(-2,1,-1,1),
-    p = new AsciiCanvas(195,65),
+    c = new AsciiCanvas(195,65),
     i = 1;
 
 var run = function () {
-  m.compute(i, p);
-  p.display();
+  m.compute(i, c);
+  c.display();
   i+=1;
   if (i <= 50) {
     setTimeout(run, 250);
